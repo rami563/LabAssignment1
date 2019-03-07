2036080b584980adfd780f639f0eeada689eb3c7
santsaxena <ags51197@gmail.com>
2019-03-07 15:56:41 -0800

    Adding the period.

--- HelloWorld.ts
+++ HelloWorld.ts
@@ -6,7 +6,7 @@ class HelloWorld {
     }
 
     sayHello() {
-        console.log('Hello World!' + ' My name is ' + this.firstName + ' ' + this.lastName);
+        console.log('Hello World!' + ' My name is ' + this.firstName + ' ' + this.lastName + '.');
 
     }
 }
